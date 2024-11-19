2f8e1186bbd0f9f14e2145ca46abbd6262c717e2
JasonAlva <alvajasonofficial@gmail.com>
2024-11-19 22:44:05 +0530

    removed key

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,9 +4,6 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-// const apiKey = process.env.GOOGLE_API_KEY;
-// const apiKey = "AIzaSyB4iPnw-x2kTXr8TWsB0eNAyeoKuf5-rU0";
-
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
